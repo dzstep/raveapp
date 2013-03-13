3f161d645f4aa715579940bf88b14d41b1b93b83
David Stepania <dzstep@gmail.com>
2013-03-12 23:43:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raveapp::Application.config.secret_token = '5eddb161435c6704d9282b8987cc56ddde78d17d590f7c53a67907aec550a1f51b69b3c07fd5aaab9b1c78aaffa5f9299bb091ad44f4328ee7c6c2c495a0e34f'
